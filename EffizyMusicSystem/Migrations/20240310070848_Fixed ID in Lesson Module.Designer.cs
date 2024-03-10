d73c20a93c5724ca1020be892de42bd635a93fe7
Raman Marwaha <ramandeepmarwaha99@gmail.com>
2024-03-10 00:10:21 -0700

    Update in Migrations

--- EffizyMusicSystem/Migrations/20240310070848_Fixed ID in Lesson Module.Designer.cs
+++ EffizyMusicSystem/Migrations/20240310070848_Fixed ID in Lesson Module.Designer.cs
@@ -4,6 +4,7 @@ using EffizyMusicSystem.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EffizyMusicSystem.Migrations
 {
     [DbContext(typeof(EffizyMusicContext))]
-    partial class EffizyMusicContextModelSnapshot : ModelSnapshot
+    [Migration("20240310070848_Fixed ID in Lesson Module")]
+    partial class FixedIDinLessonModule
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
