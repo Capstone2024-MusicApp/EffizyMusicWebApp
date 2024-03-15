03473e8ae291d74cc2e36a409ee5f3ad9a1bf67c
nicolesaldo <60759590+nicolesaldo@users.noreply.github.com>
2024-03-14 19:30:49 -0700

    Update EffizyMusicApplicationService.cs

--- EffizyMusicSystem/Services/EffizyMusicApplicationService.cs
+++ EffizyMusicSystem/Services/EffizyMusicApplicationService.cs
@@ -1,5 +1,6 @@
 ﻿using EffizyMusicSystem.DAL;
 using EffizyMusicSystem.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using System;
 using System.Collections.Generic;
@@ -26,9 +27,21 @@ namespace EffizyMusicSystem.Services
             return _context.Lessons.ToList();
         }
 
+        //public List<Payment> GetPayments()
+        //{
+        //    return _context.Payments.ToList();
+        //}
+        readonly EffizyMusicContext _dbContext;
         public List<Payment> GetPayments()
         {
-            return _context.Payments.ToList();
+            try
+            {
+                return _dbContext.Payments.ToList();
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         //Add your methods here that directly connects to the dtabase
