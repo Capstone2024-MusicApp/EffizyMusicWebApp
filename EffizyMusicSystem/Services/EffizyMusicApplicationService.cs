660fd3d62170dec2378c1d47ca6cd578d5c8d83f
etolenti <104047158+etolenti@users.noreply.github.com>
2024-03-19 18:44:26 -0600

    CourseMode to CourseCode changes
    Corresponding changes for successful build after migration

--- EffizyMusicSystem/Services/EffizyMusicApplicationService.cs
+++ EffizyMusicSystem/Services/EffizyMusicApplicationService.cs
@@ -518,7 +518,7 @@ namespace EffizyMusicSystem.Services
         #region Student Courses
         public async Task<List<StudentCourseDTO>> GetEnrolledCourses(int studentID)
         {
-            return await _context.Database.SqlQuery<StudentCourseDTO>($"select e.EnrollmentID, c.CourseID, Title , CourseDescription, CourseMode, StudentID, ProgressStatus from courses c inner join enrollments e on c.CourseId = e.CourseID where StudentID = {studentID};").ToListAsync();
+            return await _context.Database.SqlQuery<StudentCourseDTO>($"select e.EnrollmentID, c.CourseID, Title , CourseDescription, CourseCode, StudentID, ProgressStatus from courses c inner join enrollments e on c.CourseId = e.CourseID where StudentID = {studentID};").ToListAsync();
         }
 
         public StudentCourseDTO? GetStudentCourse(int enrollmentID)
@@ -526,7 +526,7 @@ namespace EffizyMusicSystem.Services
             StudentCourseDTO studentCourse;
 
             //var enrollment = var question = _context.Questions.Include(m => m.Quiz).Where(m => m.Id == id).FirstOrDefault();
-            studentCourse = _context.Database.SqlQuery<StudentCourseDTO>($"select e.EnrollmentID, c.CourseID, Title , CourseDescription, CourseMode, StudentID, ProgressStatus from courses c inner join enrollments e on c.CourseId = e.CourseID where EnrollmentID = {enrollmentID}").SingleOrDefault();
+            studentCourse = _context.Database.SqlQuery<StudentCourseDTO>($"select e.EnrollmentID, c.CourseID, Title , CourseDescription, CourseCode, StudentID, ProgressStatus from courses c inner join enrollments e on c.CourseId = e.CourseID where EnrollmentID = {enrollmentID}").SingleOrDefault();
 
             studentCourse.Modules = _context.Modules.Include(l=>l.Lessons).Include(q=>q.Quizzes).Where(m => m.Course.CourseID == studentCourse.CourseID).ToList();
             return studentCourse;
