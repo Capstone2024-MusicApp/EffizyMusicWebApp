9042e509e813eacb8374c3aead47d00aeccc2ebc
etolenti <104047158+etolenti@users.noreply.github.com>
2024-04-04 18:12:57 -0600

    Progress Classes

---
+++ EffizyMusicSystem/Models/LessonProgress.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffizyMusicSystem.Models
+{
+    public class LessonProgress
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LessonProgressID { get; set; }
+        public int EnrollmentID { get; set; }
+
+        public int LessonID { get; set; }
+
+        public required string ProgressStatus { get; set; }
+
+    }
+}
