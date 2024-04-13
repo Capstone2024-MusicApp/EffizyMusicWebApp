8b2c8f0a74a69297bd1adeb99739141bbf103153
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2024-04-12 23:24:51 -0600

    RemovedStudentGrades

---
+++ EffizyMusicSystem/Migrations/20240413052410_RemovedGradesFromStudent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EffizyMusicSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedGradesFromStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Grades",
+                table: "Students");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Grades",
+                table: "Students",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
